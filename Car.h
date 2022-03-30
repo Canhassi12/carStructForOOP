50baae4fc8194692c884e037c6b139bffbe26b89
Canhassi <arthurcanhassi@gmail.com>
2022-03-29 22:06:14 -0300

    update more one time

--- Car.h
+++ Car.h
@@ -1,61 +1,52 @@
-#ifndef carro_h
-#define carro_h
+#ifndef Car_h
+#define Car_h
 
 #include <iostream>
 #include <string>
 using namespace std;
 
 
-class carro
+class Car
 {
     private:
         string color;
         string prefix;
         string model;
-        int age;
+        int    age;
         double value;
 
     public:
-        void setColor(string Color)
+        Car(string color, string prefix, string model, int age, double value)
         {
-            this->color = Color;
+            this->color  = color;
+            this->prefix = prefix;
+            this->model  = model;
+            this->age    = age;
+            this->value  = value;
         }
+
+     
+                
         string getColor()
         {
             return this->color;
         }
 
-        void setPrefix(string Prefix)
-        {
-            this->prefix = Prefix;
-        }
         string getPrefix()
         {
             return this->prefix;
         }
 
-        void setMolel(string Model)
-        {
-            this->model = Model;
-        }
         string getModel()
         {
             return this->model;
         }
 
-        void setAge(int Age)
-        {
-            this->age = Age;
-        }
         int getAge()
         {
             return this->age;
         }
 
-        void setValue(double Value)
-        {
-            this->value = Value;
-        }
         double getValue()
         {
             return this->value;
@@ -68,19 +59,6 @@ class carro
             cout<<getColor()<<endl;
             cout<<getAge()<<endl;
             cout<<getValue()<<endl;
-        }
+        } 
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
 #endif
--- main.cpp
+++ main.cpp
@@ -1,104 +1,93 @@
 #include <iostream>
 #include <string>
-#include "carro.h"
+#include "Car.h"
 
 using namespace std;
 
-void setAndPrintToro()
+void printChooseMenu()
 {
-	carro c1;
-	c1.setColor("branco");
-	c1.setPrefix("Fiat");
-	c1.setMolel("Toro");
-	c1.setAge(2019);
-	c1.setValue(90.990);
-
-	c1.printCar();
+    cout << "\nOkay, lest go!!" << endl;
+    cout << "\n1: Fiat Toro..." << endl;
+    cout << "\n2: Audi A4..." << endl;
+    cout << "\n3: Golf Gti..." << endl;
+    cout << "\n4: Uninho de firma..." << endl;
+	cout << "\n5: Create a new car" << endl;
 }
 
-void setAndPrintAudi()
+void newcar(string nColor, string nPrefix, string nModel,int nAge, double nValue)
 {
-	carro c2;
-	c2.setColor("preto");
-	c2.setPrefix("Audi");
-	c2.setMolel("A4");
-	c2.setAge(2021);
-	c2.setValue(242.990);
-
-	c2.printCar();
+	Car newCar(nColor, nPrefix, nModel, nAge, nValue );
 }
 
-void setAndPrintGolf()
-{
-	carro c3;
-	c3.setColor("preto, branco");
-	c3.setPrefix("Wolkswagen");
-	c3.setMolel("Golf GTI");
-	c3.setAge(2019);
-	c3.setValue(143.790);
-
-	c3.printCar();
-}
-
-void setAndPrintUno()
-{
-	carro c4;
-	c4.setColor("azul");
-	c4.setPrefix("Fiat");
-	c4.setMolel("Uno de firma");
-	c4.setAge(1995);
-	c4.setValue(7.000);
-
-	c4.printCar();
-}
 int main()
 {
-	int escolha;
-	string resposta;
+    int choice;
+    string answer;
+	string nColor;
+	string nPrefix;
+	string nModel;
+	int    nAge;
+	double nValue;
+
+    cout << "Welcome to the car shop!" << endl;
+    cout << "\nDo you want buy or create a car ? Yes/No" << endl;
+    cin >> answer;
+
+    if (answer != "yes") {
+        if (answer == "no") {
+            cout << "No problem, come back anytime! :)\n";
+            return 0;
+        }
+        cout << "I'm sorry but we don't have that option :/\n";
+        return 128;
+    }
+
+    printChooseMenu();
+    cin >> choice;
+
+    Car toro("White", "Fiat", "Toro", 2019, 90990);
 	
-	cout << "Welcome to the car shop" << endl; ///apresenta��o
-	cout << "\ndo you want buy a car ? Yes/No" << endl;
-	cin >> resposta;
+	Car audi("Black", "Audi", "A4", 2021, 242990);
 
-	if(resposta == "yes")
-	{
-		cout << "\nOkay, lest go!!" << endl;
+	Car golfGti("Black, White", "Volkswagen", "Golf GTI", 2019, 143790);
+	
+	Car uno("Blue", "Fiat", "Uno de firma", 1995, 7000);
 
-		cout << "\nWe have thats models: " << endl;
-
-		cout << "\n1: Fiat Toro..." << endl;
-		cout << "\n2: Audi A4..." << endl;
-		cout << "\n3: Golf Gti..." << endl;
-		cout << "\n4: Uninho de firma..." << endl;
-		cin>>escolha;
-
-		switch(escolha)
+	switch(choice) 
 		{
-			case 1: setAndPrintToro();
-
+			case 1:
+				toro.printCar();
 				break;
 
-			case 2: setAndPrintAudi();
-
+			case 2:
+				audi.printCar();
 				break;
 
-			case 3: setAndPrintGolf();
-
-				break; 
-
-			case 4: setAndPrintUno();
-
+			case 3:
+				golfGti.printCar();
 				break;
+
+			case 4:
+				uno.printCar();
+				break;
+			
+			case 5:
+				cout<< "ok, first write the Model" <<endl;
+				cin>> nModel;
+				cout<< "\nBrand" <<endl;
+				cin>>nPrefix;
+				cout<< "\nColor" <<endl;
+				cin>>nColor;
+				cout<< "\nYear" <<endl;
+				cin>>nAge;
+				cout<< "\nValue" <<endl;
+				cin>>nValue;
+				newcar(nColor, nPrefix, nModel, nAge, nValue);
+				break;
+			
+        default:
+            cout << "\nWe have thats models: " << endl;
+            break;
 		}
-	}
-	else if(resposta=="no")
-	{
-		cout<<"No problem, comeback anytime"<<endl;
-	}
-	else
-	{
-		cout<<"\nwe dont have that options"<<endl;
-	}
 
-	return 0;
-}
+}	 
